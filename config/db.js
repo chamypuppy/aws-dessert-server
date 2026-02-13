104f0bcc67d2724beccca055b841683d4a56e95c
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2026-02-13 14:23:31 +0900

    Update db.js
    - 자원 낭비로 인한 process.exit(1) 삭제
    - Connection Pool 설정: DB 미 사용 시 타임아웃되는 불편함 개선

--- config/db.js
+++ config/db.js
@@ -1,13 +1,15 @@
 const mysql = require('mysql2');
 const dbConfig = require('./db.config');
-const db = mysql.createConnection(dbConfig);
+// const db = mysql.createConnection(dbConfig);
 
-db.connect(err => {
+const db = mysql.createPool(dbConfig);
+
+db.getConnection((err, connection) => {
   if (err) {
-    console.error('💦MySQL 연결에 실패하였습니다!: \n', err);
-    process.exit(1);
+    console.error('⚡: MySQL 커넥션 풀 연결에 실패하였습니다!\n', err);
   } else {
-    console.log('MySQL에 연결되었습니다.');
+    console.log('😄: MySQL Connection Pool이 설정되었습니다.');
+    connection.release();
   }
 });
 
