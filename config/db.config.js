5d442dac751eb78f2b1e36906333bab6d79b84f2
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2026-02-13 14:15:25 +0900

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -5,7 +5,7 @@ const dbConfig = {
   user: process.env.DB_USER,         // 환경 변수에서 DB_USER 가져오기
   password: process.env.DB_PASSWORD, // 환경 변수에서 DB_PASSWORD 가져오기
   database: process.env.DB_NAME,     // 환경 변수에서 DB_NAME 가져오기
-  port: process.env.DB_PORT          // MySQL 포트!
+  port: process.env.DB_PORT,          // MySQL 포트!
 
   waitForConnections: true,          // 연결 다 차면 대기
   connectionLimit: 9,                // 동시에 유지할 연결 수
